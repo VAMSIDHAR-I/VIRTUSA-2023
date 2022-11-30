2c619529b7cd38b89b789fb32865ebc01ff0924f
VAMSIDHAR-I <105840664+VAMSIDHAR-I@users.noreply.github.com>
2022-11-30 08:16:00 +0530

    Add files via upload

---
+++ Age.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Age{
+public static void main(String[] args){
+int age=17;
+if(age>=18){
+System.out.println("valid");
+}
+else{
+System.out.println("eligible to vote after'"+(18-age)+"'years");
+}
+}
+}
